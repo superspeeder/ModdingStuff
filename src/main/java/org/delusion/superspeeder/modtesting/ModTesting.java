418851355fc652ef309361460ea685fc57c58f9c
superspeeder <andy@delusion.org>
2017-06-02 14:53:45 -0700

    Create ModTesting.java

---
+++ src/main/java/org/delusion/superspeeder/modtesting/ModTesting.java
@@ -0,0 +1,19 @@
+package org.delusion.superspeeder.modtesting;
+
+// Imports Here
+
+
+@Mod(modid="modtesting", name="Mod Testing", version="0.0.0.01")
+public class ModTesting {
+  public void init(FMLInitializationEvent event) {
+    // inits here
+  }
+  
+  public void preInit(FMLPreInitializationEvent event) {
+    // pre-inits here
+  }
+  
+  public void postInit(FMLPostInitializationEvent event) {
+    // post-inits here
+  }
+}
